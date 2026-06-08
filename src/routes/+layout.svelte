cd3ea2507a61ea64c39bbba55f874b13eabdab43
d1vij <divij@divijverma.com>
2026-06-08 09:59:42 +0530

    chore: add fonts preloading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,22 @@
     let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+    <link rel="icon" href={favicon} />
+
+    <link
+        rel="preload"
+        href="/fonts/UntitledSans/ttf/UntitledSans-Regular.ttf"
+        as="font"
+        type="font/ttf"
+        crossorigin=""
+    />
+    <link
+        rel="preload"
+        href="/fonts/UntitledSerif/ttf/UntitledSerif-Regular.ttf"
+        as="font"
+        type="font/ttf"
+        crossorigin=""
+    />
+</svelte:head>
 {@render children()}
